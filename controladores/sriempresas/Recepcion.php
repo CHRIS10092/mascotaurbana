4f1a50474e2e4bd8053678be79bc1c996217a1ec
CHRIS10092 <koriche001@gmail.com>
2022-07-21 15:46:50 -0500

    okrpint

--- controladores/sriempresas/Recepcion.php
+++ controladores/sriempresas/Recepcion.php
@@ -3,3 +3,8 @@
 session_start();
 print_r($_SESSION);
 print_r($_POST);
+$idempresa=$_SESSION['empresa']['idempresa'];
+$idsucursal=$_SESSION['sucursal']['codigo'];
+print_r($idempresa);
+print_r($idsucursal);
+?>
